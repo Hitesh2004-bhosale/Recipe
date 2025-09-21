005b70a1d53b4f08a858e2bf66a2cc742db21b5f
Hitesh2004-bhosale <bhosalehitesh7722@gmail.com>
2025-09-21 22:35:41 +0530

    Remove duplicate neon import in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,9 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { ENV } from "./env.js";
 import * as schema from "../db/schema.js";
 
 // Create Neon HTTP client
-//const sql = neon(ENV.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL);
 
 // Initialize Drizzle ORM with schema
 export const db = drizzle(sql, { schema });
-
-
-
-
-
-
-
-import { neon } from '@neondatabase/serverless';
-
-const sql = neon(process.env.DATABASE_URL);
-
-export default sql;
